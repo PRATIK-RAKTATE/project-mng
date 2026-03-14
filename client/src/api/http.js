2d073d566763ff5847a0f2169725b6c36e82ce4c
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2026-03-14 07:46:07 +0530

    add sw file in public

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,5 +1,5 @@
 const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
-
+console.log(API_BASE)
 async function request(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
     headers: {
